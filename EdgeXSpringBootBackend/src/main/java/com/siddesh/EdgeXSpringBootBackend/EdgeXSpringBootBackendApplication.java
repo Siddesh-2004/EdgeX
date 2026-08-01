f704bb6014020afa2103ad469a32e1971b0b572d
siddesh <siddeshpoojary2004@gmail.com>
2026-08-01 07:15:54 +0530

    setup: add project structure

---
+++ EdgeXSpringBootBackend/src/main/java/com/siddesh/EdgeXSpringBootBackend/EdgeXSpringBootBackendApplication.java
@@ -0,0 +1,13 @@
+package com.siddesh.EdgeXSpringBootBackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EdgeXSpringBootBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EdgeXSpringBootBackendApplication.class, args);
+	}
+
+}
